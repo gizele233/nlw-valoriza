c3f445dd9325d64e4260df4d7d23cd289955f776
gizele233 <gizele233@gmail.com>
2021-06-28 00:13:26 -0300

    adição do cors

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "./database";
 import {router} from "./routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
